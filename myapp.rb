64eaf3c0e082b57b05d441b9f706ff1111c62286
Ben <ben.dalton@madetech.com>
2020-10-01 09:32:15 +0100

    using params in route pattern, not working yet

--- myapp.rb
+++ myapp.rb
@@ -3,3 +3,13 @@ require 'sinatra'
 get '/' do
     "Hello world!"
 end
+
+# using params in route pattern
+
+params = {:name => "foo"}
+
+# puts params['name']
+
+# get '/hello/:name' do
+#     "Hello #{params['name']}"
+# end
